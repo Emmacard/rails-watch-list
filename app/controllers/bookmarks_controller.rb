6fb14cb1135fe72b2e5ab483d9b7a37c8e199d94
Emma Cardon <emma.cardon7@gmail.com>
2021-05-28 17:54:54 +0100

    app all stet up

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,16 +1,35 @@
 class BookmarksController < ApplicationController
+  before_action :find_list, only: [:new, :create]
   def new
-    @bookmark = Bookmark.new(bookmark_params)
-    @movie = Movie.find(params[:movie_id])
+    @bookmark = Bookmark.new
   end
 
-  # def method_name
+  def create
+      # list_id = params[:list_id]
+      @bookmark = Bookmark.new(bookmark_params)
+      # @bookmark.list_id = list_id
+      @bookmark.list = @list
+      if @bookmark.save
+        redirect_to list_path(@list)
+      else
+        render :new
+      end
+  end
 
-  # end
+  def destroy
+    bookmark = Bookmark.find(params[:id])
+    list = bookmark.list
+    bookmark.destroy
+    redirect_to list_path(list)
+  end
 
-  # private
+  private
 
-  def bookmark
-    params.require(:bookmark).permit(:comment)
+  def find_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
